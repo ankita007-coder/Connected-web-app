c4cf991acab4ecfaa645035d7f28446c714ec17e
ankita007-coder <ankitabudhia42@gmail.com>
2023-05-24 17:10:57 +0530

    corrected error for google authentication

--- config/passport-google-oauth2-strategy.js
+++ config/passport-google-oauth2-strategy.js
@@ -1,5 +1,5 @@
 const passport = require('passport');
-const googleStrategy = require('passport-google-strategy').OAuth2Strategy;
+const googleStrategy = require('passport-google-oauth').OAuth2Strategy;
 const crypto = require('crypto');
 
 const User = require('../models/user');
@@ -7,7 +7,7 @@ const User = require('../models/user');
 passport.use(new googleStrategy({
     clientID: "919048359936-1rr0jg1u1t60javu5viobus98so27u76.apps.googleusercontent.com",
     clientSecret: "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",
-    callbackURL: "https://localhost:8000/users/google/callback"
+    callbackURL: "http://localhost:8000/users/auth/google/callback"
     },
     function(accessToken, refreshToken, profile, done){
         //find a user
@@ -26,7 +26,7 @@ passport.use(new googleStrategy({
                 //if not found, create a new one and set it as req.user
                 User.create({
                     name: profile.displayName,
-                    email: profile.email[0].value,
+                    email: profile.emails[0].value,
                     password: crypto.randomBytes(20).toString('hex')
                 },function(err, user){
                     if(err){
